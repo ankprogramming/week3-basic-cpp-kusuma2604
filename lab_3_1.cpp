bee82c748e7684d628949e21da46767a1ce11539
Unknown <oppof1szaa168@gmail.com>
2017-11-27 18:09:55 +0700

    ทำโจทย์ข้อที่ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Mr.kusuma " <<endl;
+  cout << "last names sonchaum" <<endl;
+  cout << "nickname Fah" <<endl;
+  cout << "sex female" <<endl;
+  cout << "Birthday 26 April 1999 " <<endl;
+  cout << "Hobbies facebook " <<endl;
+  cout << "Status single" <<endl;
   return 0;
 }
